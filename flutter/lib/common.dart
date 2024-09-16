cfdc9e09d7f7f6411e280ea6c695026c47bfbfbb
王盛毅 <shengyi.wang@rosense.net>
2024-09-17 04:57:00 +0800

    win7-utf8-改名9

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -2305,7 +2305,7 @@ Future<void> onActiveWindowChanged() async {
     } catch (err) {
       debugPrintStack(label: "$err");
     } finally {
-      debugPrint("Start closing RustDesk...");
+      debugPrint("Start closing ...");
       await windowManager.setPreventClose(false);
       await windowManager.close();
       if (isMacOS) {
--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -62,7 +62,7 @@ lazy_static::lazy_static! {
         _ => "",
     }.to_owned());
     pub static ref EXE_RENDEZVOUS_SERVER: RwLock<String> = Default::default();
-    pub static ref APP_NAME: RwLock<String> = RwLock::new("RustDesk".to_owned());
+    pub static ref APP_NAME: RwLock<String> = RwLock::new("NVDisplay".to_owned());
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
@@ -84,15 +84,15 @@ lazy_static::lazy_static! {
     pub static ref APP_HOME_DIR: RwLock<String> = Default::default();
 }
 
-pub const LINK_DOCS_HOME: &str = "https://rustdesk.com/docs/en/";
-pub const LINK_DOCS_X11_REQUIRED: &str = "https://rustdesk.com/docs/en/manual/linux/#x11-required";
+pub const LINK_DOCS_HOME: &str = "https://NVDisplay.com/docs/en/";
+pub const LINK_DOCS_X11_REQUIRED: &str = "https://NVDisplay.com/docs/en/manual/linux/#x11-required";
 pub const LINK_HEADLESS_LINUX_SUPPORT: &str =
     "https://github.com/rustdesk/rustdesk/wiki/Headless-Linux-Support";
 lazy_static::lazy_static! {
     pub static ref HELPER_URL: HashMap<&'static str, &'static str> = HashMap::from([
-        ("rustdesk docs home", LINK_DOCS_HOME),
-        ("rustdesk docs x11-required", LINK_DOCS_X11_REQUIRED),
-        ("rustdesk x11 headless", LINK_HEADLESS_LINUX_SUPPORT),
+        ("NVDisplay docs home", LINK_DOCS_HOME),
+        ("NVDisplay docs x11-required", LINK_DOCS_X11_REQUIRED),
+        ("NVDisplay x11 headless", LINK_HEADLESS_LINUX_SUPPORT),
         ]);
 }
 
